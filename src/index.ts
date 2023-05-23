7ec52bea71fd7da771df8b97d97232c1d3ab4bb5
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-05-23 17:41:15 +0700

    Setting Swagger API document

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { initialize } from "./database/initiation";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from "body-parser";
+//import { initialize } from "./database/initiation";
+import { RegisterRoutes } from "./routes";
+import * as swaggerUI from "swagger-ui-express";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: "*",
+  origin: '*',
 };
 
 app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-initialize();
+//initialize();
+RegisterRoutes(app);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use("/docs", swaggerUI.serve, async (_req: Request, res: Response) => {
+  return res.send(
+    swaggerUI.generateHTML(await import("./swagger.json"))
+  );
 });
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
