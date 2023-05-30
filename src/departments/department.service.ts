85b4ab98e4bb6e2395906f28f3235c77645980e8
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-05-30 17:32:35 +0700

    List, ListOne, Create, Update, Delete (if no document) Room

--- src/departments/department.service.ts
+++ src/departments/department.service.ts
@@ -1,16 +1,26 @@
 import { Department } from './entities/department.entity';
 import { AppDataSource } from '../database/data-source';
-import { DepartmentDto, CreateDepartmentDto } from './dtos/department.dto';
+import {
+  UpdateDepartmentDto,
+  CreateDepartmentDto,
+} from './dtos/department.dto';
 import { UUID } from 'type/global';
 import { singleton } from 'tsyringe';
+import { Repository } from 'typeorm';
 
 @singleton()
 export class DepartmentService {
+  private departmentRepository: Repository<Department>;
+
+  constructor() {
+    this.departmentRepository = AppDataSource.getRepository(Department);
+  }
+
   public async getOne(id: UUID) {
     try {
-      return await AppDataSource.getRepository(Department).findOne({
+      return await this.departmentRepository.findOne({
         where: {
-          id: id
+          id: id,
         },
       });
     } catch (error) {
@@ -18,20 +28,30 @@ export class DepartmentService {
       return null;
     }
   }
+
   public async getAll() {
-    return await AppDataSource.getRepository(Department).find();
+    return await this.departmentRepository.find();
   }
 
-  public async create(department: CreateDepartmentDto) {
-    return await AppDataSource.getRepository(Department).save(department);
-  }
-
-  public async update(department: DepartmentDto) {
+  public async create(departmentDto: CreateDepartmentDto) {
     try {
-      const result = await AppDataSource.getRepository(Department).update({
-        id: department.id
-    }, department)
-    return result.affected === 1;
+      const department = this.departmentRepository.create(departmentDto);
+      return await this.departmentRepository.save(department);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  public async update(department: UpdateDepartmentDto) {
+    try {
+      const result = await this.departmentRepository.update(
+        {
+          id: department.id,
+        },
+        department
+      );
+      return result.affected === 1;
     } catch (error) {
       console.log(error);
       return false;
@@ -40,7 +60,7 @@ export class DepartmentService {
 
   public async delete(id: string) {
     const result = await AppDataSource.getRepository(Department).delete({
-      id: id
+      id: id,
     });
     return result.affected === 1;
   }
--- src/departments/entities/department.entity.ts
+++ src/departments/entities/department.entity.ts
@@ -1,14 +1,20 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+import { Room } from '../../rooms/entities/room.entity';
 
 @Entity()
 export class Department {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
   name: string;
 
   @OneToMany(() => User, (user) => user.department)
   users: User[];
+
+  @OneToMany(() => Room, (room) => room.department)
+  rooms: Room[];
 }
