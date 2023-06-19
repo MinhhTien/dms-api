d596c79fd48ffd66d60a3119809ff0a7a0dc9d5f
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-06-19 08:20:41 +0700

    Add photoURL in User response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,13 +5,22 @@ import {
   Security,
   Tags,
   Request,
+  Path,
 } from 'tsoa';
 import { User } from './entities/user.entity';
 import { SuccessResponse, BadRequestError } from '../constants/response';
+import { injectable } from 'tsyringe';
+import { UsersService } from './users.service';
+import { UUID } from '../lib/global.type';
 
+@injectable()
 @Tags('User')
 @Route('users')
 export class UsersController extends Controller {
+  constructor(private userService: UsersService) {
+    super();
+  }
+
   @Security('api_key', ['STAFF', 'EMPLOYEE'])
   @Get('own')
   public getUser(
@@ -19,6 +28,7 @@ export class UsersController extends Controller {
   ): User {
     return request.user;
   }
+
   @Security('api_key', ['STAFF', 'EMPLOYEE'])
   @Get('login')
   public login(
@@ -26,5 +36,15 @@ export class UsersController extends Controller {
   ): SuccessResponse | BadRequestError {
     if(request.user) return new SuccessResponse('Login success', null);
     else return new BadRequestError('Login fail');
+  }
+
+  //@Security('api_key', ['STAFF', 'EMPLOYEE'])
+  @Get('profile/:id')
+  public async getProfile(
+    @Path() id: UUID,
+  ) {
+    const result = await this.userService.getProfile(id);
+    if(result !== null) return new SuccessResponse('Success', result);
+    else return new BadRequestError('Fail to get profile');
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,10 @@
 import { User } from './entities/user.entity';
 import { AppDataSource } from '../database/data-source';
 import { UserStatus } from '../constants/enum';
+import { getAuth } from 'firebase-admin/auth';
+import { singleton } from 'tsyringe';
+import { Repository } from 'typeorm';
+import { UUID } from '../lib/global.type';
 
 // A post request should not contain an id.
 export type CreateUserDto = Pick<
@@ -8,14 +12,54 @@ export type CreateUserDto = Pick<
   'email' | 'lastName' | 'firstName' | 'phone'
 >;
 
+export type UserProfile = User & {
+  photoURL?: string;
+};
+
+@singleton()
 export class UsersService {
-  public async get(email: string): Promise<User | null> {
-    return await AppDataSource.getRepository(User).findOne({
+  private userRepository: Repository<User>;
+
+  constructor() {
+    this.userRepository = AppDataSource.getRepository(User);
+  }
+
+  public async get(email: string): Promise<UserProfile | null> {
+    const user = await this.userRepository.findOne({
       where: {
         email: email,
         status: UserStatus.ACTIVE,
       },
       relations: ['role', 'department'],
     });
+    if (user === null) return null;
+    const userRecord = await getAuth().getUserByEmail(user.email);
+    return {
+      photoURL: userRecord.photoURL,
+      ...user,
+    };
+  }
+
+  public async getProfile(id: UUID): Promise<UserProfile | null> {
+    try {
+      const user = await this.userRepository.findOne({
+        where: {
+          id: id,
+          status: UserStatus.ACTIVE,
+        },
+        relations: ['role', 'department'],
+      });
+      if (user === null) return null;
+      console.log(user);
+      const userRecord = await getAuth().getUserByEmail(user.email);
+      console.log(`Successfully fetched user data: ${userRecord.photoURL}`);
+      return {
+        photoURL: userRecord.photoURL,
+        ...user,
+      };
+    } catch (error) {
+      console.log('Error fetching user data:', error);
+      return null;
+    }
   }
 }
