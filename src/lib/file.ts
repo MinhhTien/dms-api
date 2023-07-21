42345c6eb6f0d7cd4fab4819b3bfe143355ede98
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-07-21 11:05:54 +0700

    Optimize duplicate

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -1,13 +1,13 @@
-//const combineImage = require('combine-image');
+const combineImage = require('combine-image');
 import pdfImgConvert from 'pdf-img-convert';
 import fs from 'fs';
 import { resolve } from 'path';
 import Jimp from 'jimp';
 import { UUID } from './global.type';
 
-export const convert = async (fileName: string) => {
+export const convertFirstPage = async (fileName: string) => {
   try {
-    console.log(`Converting ${fileName} ...`);
+    console.log(`Converting first page ${fileName} ...`);
     const name = fileName.split('.')[0];
     const pngFileName = resolve('temp', `${name}.png`);
     if (!fs.existsSync('temp')) fs.mkdirSync('temp');
@@ -28,7 +28,58 @@ export const convert = async (fileName: string) => {
     if (outputImages.length === 0) return;
     fs.writeFileSync(`temp/${name}.png`, outputImages[0]);
 
-    console.log(`Convert ${fileName} success!`);
+    console.log(`Convert first page ${fileName} success!`);
+  } catch (error) {
+    console.log('Convert: ' + error);
+  }
+};
+
+export const convertAll = async (fileName: string) => {
+  try {
+    console.log(`Converting all ${fileName} ...`);
+    const name = fileName.split('.')[0];
+    const pngFileName = resolve('temp', `${name}.png`);
+    if (!fs.existsSync('temp')) fs.mkdirSync('temp');
+
+    // if png file already exist, return
+    if (fs.existsSync(pngFileName)) return;
+
+    const pdfFilePath = resolve('uploads', fileName);
+    if (!fs.existsSync(pdfFilePath)) {
+      console.log('convert::' + pdfFilePath + 'File not exist');
+      return;
+    }
+
+    const outputImages = await pdfImgConvert.convert(pdfFilePath);
+    const tempFileNameList: string[] = [];
+    const writeFilePromise = [];
+    let tempFileName = '';
+
+    console.log('outputImages length: ', outputImages.length)
+    for (let i = 0; i < outputImages.length; i++) {
+      tempFileName = `temp/${name}` + i + '.png';
+      tempFileNameList.push(tempFileName);
+      writeFilePromise.push(
+        fs.writeFile(tempFileName, outputImages[i], function (error) {
+          if (error) {
+            console.error('Error: ' + error);
+          }
+        })
+      );
+    }
+    await Promise.all(writeFilePromise);
+    const result = await combineImage(tempFileNameList, {
+      direction: 'row',
+    });
+    
+    result.write(`temp/${name}.png`, () => {
+      console.log('Converted all success!');
+      tempFileNameList.forEach((fileName) => {
+        fs.unlink(fileName, (err) => {
+          if (err) console.log(err);
+        });
+      });
+    });
   } catch (error) {
     console.log('Convert: ' + error);
   }
